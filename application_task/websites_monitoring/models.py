19f5ad6c77f3e087161ea00fcb1de3ba7ab6f147
clement <clement.daubrenet@gmail.com>
2016-08-21 18:36:17 +0200

    Backend and models ok

--- application_task/websites_monitoring/models.py
+++ application_task/websites_monitoring/models.py
@@ -1,5 +1,12 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Sites(models.Model):
+	
+    site_name =  models.CharField(max_length=200,default="-")
+
+class Values(models.Model):
+
+    site =  models.ForeignKey('Sites')
+    date =  models.DateField(default="-")
+    value_a = models.FloatField(default=0)
+    value_b = models.FloatField(default=0)
