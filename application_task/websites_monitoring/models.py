b66b4002c647fae8a7539390b4a875a33b83c1f4
clement <clement.daubrenet@gmail.com>
2016-08-23 08:13:40 +0200

    Better documentation

--- application_task/websites_monitoring/models.py
+++ application_task/websites_monitoring/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Sites(models.Model):
 	"""
-	This model is for websites names and ids.
+	This model represents websites names and ids.
 	n.b: primary key is set up automatically.
 	"""
 	site_name =  models.CharField(max_length=200,default="-")
 
 class Values(models.Model):
 	"""
-	This model is for the date and the values 
+	This model represents the dates and the values 
 	for a given website. 
 	"""
 	site =  models.ForeignKey('Sites')
