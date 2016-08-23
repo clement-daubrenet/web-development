1e96b2da1a5d9e39d375af957d25115a4b3007b4
clement <clement.daubrenet@gmail.com>
2016-08-23 04:09:57 +0200

    udpate models

--- application_task/websites_monitoring/models.py
+++ application_task/websites_monitoring/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class Sites(models.Model):
-	
-    site_name =  models.CharField(max_length=200,default="-")
+	"""
+	This model is for websites names and ids.
+	n.b: primary key is set up automatically.
+	"""
+	site_name =  models.CharField(max_length=200,default="-")
 
 class Values(models.Model):
-
-    site =  models.ForeignKey('Sites')
-    date =  models.DateField(default="-")
-    value_a = models.FloatField(default=0)
-    value_b = models.FloatField(default=0)
+	"""
+	This model is for the date and the values 
+	for a given website. 
+	"""
+	site =  models.ForeignKey('Sites')
+	date =  models.DateField(default="-")
+	value_a = models.FloatField(default=0)
+	value_b = models.FloatField(default=0)
