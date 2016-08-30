dbc77fec8fdd91298d14a7f6e3741983f83dbb2f
cd <clement.daubrenet@gmail.com>
2016-08-30 15:02:58 +0200

    update/refacto

--- kafka/simpleconfluent.py
+++ kafka/simpleconfluent.py
@@ -4,52 +4,61 @@ import time
 import uuid
 import sys
 
-def producer():
+def producer(broker_name, topic_name, number_of_messages):
+    """
+    This fucntion instantiates a producer
+    :topic_name: name of the topic
+    e.g : test_topic
+    :broker_name: name of the broker
+    e.g: localhost:9092
+    """
 
-    conf = {'bootstrap.servers': 'localhost:9092'}
+    conf = {'bootstrap.servers': broker_name}
     p = confluent_kafka.Producer(**conf)
-    topic = 'test'
-    msgcnt = 1
-    msg_pattern = 'pattern sent'
-    t_produce_start = time.time()
+    topic = topic_name
+    messages_number_to_send = number_of_messages
     msgs_produced = 0
-    msgs_backpressure = 0
-    print('# producing %d messages to topic %s' % (msgcnt, topic))
-    for i in range(0, msgcnt):
-        p.produce('test', value=msg_pattern)
+    message = 'pattern sent'
+    print('# producing %d messages to topic %s' % (messages_number_to_send, topic))
+    for _ in range(0, messages_number_to_send):
+        p.produce(topic_name, value=message.encode('utf8'))
         msgs_produced += 1
         p.poll(0)
-
-    t_produce_spent = time.time() - t_produce_start
     print('waiting for %d/%d deliveries' % (len(p), msgs_produced))
     # Wait for deliveries
     p.flush()
-    t_delivery_spent = time.time() - t_produce_start
 
-
-def consumer():
-    """ Verify Consumer performance """
-
-    conf = {'bootstrap.servers': 'localhost9092',
-            'group.id': uuid.uuid1()}
-
+def consumer(broker_name, topic_name, number_of_messages):
+    """
+    This function consumes based on a
+    broker name and a topic name.
+    """
+    conf = {'bootstrap.servers': broker_name,
+            'group.id': uuid.uuid1(),
+            'session.timeout.ms': 6000,
+            'default.topic.config': {
+                'auto.offset.reset': 'earliest'
+            }}
     c = confluent_kafka.Consumer(**conf)
-    c.subscribe(["test"])
-    max_msgcnt = 1
+    c.subscribe([topic_name])
+    max_msgcnt = number_of_messages
     msgcnt = 0
     print('Will now consume %d messages' % max_msgcnt)
     while True:
         # Consume until EOF or error
         msg = c.poll()
+        if msgcnt == 0:
+            t_start = time.time()
         msgcnt += 1
-        if msgcnt == 1:
-            t_first_msg = time.time()
         if msgcnt >= max_msgcnt:
             break
-    t_spent = time.time() - t_first_msg
+    t_spent = time.time() - t_start
     print('consumed in %.10fs' % \
           t_spent)
     c.close()
 
-producer()
-consumer()
+broker_name = "prod-cruncher-broker-1.apps.artefact.is:6667"
+topic_name = "test_clement_kafka"
+number_of_messages = 500
+producer(broker_name, topic_name, number_of_messages)
+consumer(broker_name, topic_name, number_of_messages)
