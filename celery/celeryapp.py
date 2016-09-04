df7724ce60ab1739191e6dd57e50ab207785d6bc
clement <clement.daubrenet@gmail.com>
2016-09-05 00:04:00 +0200

    Update. Http calls to the rest API to do

--- celery/celeryapp.py
+++ celery/celeryapp.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.task import http
 
 # 1. Install RABITMQ to get a message broker
 # 2. Run it through invoke-rc.d rabbitmq-server start 
@@ -6,6 +7,11 @@ from celery import Celery
 # See doc Celery online
 
 app = Celery('celeryapp', broker='amqp://guest@localhost//')
+
 @app.task
-def add(x, y):
-    return x + y
+def weather(city_id):
+    base_url = "http://api.openweathermap.org/data/2.5/forecast/"
+    daily_url = "daily?id={}&APPID=64af4014d4d7321058115459dfc67525".format(city_id)
+    url = base_url + daily_url
+    #http.make_request(url, method="get",{"":""})
+    
