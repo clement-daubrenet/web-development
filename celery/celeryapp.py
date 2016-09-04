28e8dbb3ef339496e081848c423f60ddd710cc13
clement <clement.daubrenet@gmail.com>
2016-09-04 20:39:06 +0200

    Celery quickstart working

--- celery/celeryapp.py
+++ celery/celeryapp.py
@@ -1,7 +1,11 @@
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://guest@localhost//')
+# 1. Install RABITMQ to get a message broker
+# 2. Run it through invoke-rc.d rabbitmq-server start 
+# 3. launch celery -A celeryapp worker --loglever=info
+# See doc Celery online
 
+app = Celery('celeryapp', broker='amqp://guest@localhost//')
 @app.task
 def add(x, y):
     return x + y
