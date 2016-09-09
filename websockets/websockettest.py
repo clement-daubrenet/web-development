20bf466db0d712c339e84dcf31876afe10001cd9
clement <clement.daubrenet@gmail.com>
2016-09-09 16:00:18 +0200

    websockets, to finish : {{connected}} is empty, to correct

--- websockets/websockettest.py
+++ websockets/websockettest.py
@@ -1,13 +1,27 @@
 import redis
 from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
+from gevent import monkey
+monkey.patch_all()
 
 app = Flask(__name__)
 db = redis.StrictRedis('localhost')
+socketio = SocketIO(app) 
 
 @app.route('/')
 def main():
    c=db.incr('counter')
    return render_template("main.html", counter=c)
 
+@socketio.on("connect",namespace="/connexions")
+def ws_connect():
+    c = db.incr('connected')
+    socketio.emit('msg', {'counter': c}, namespace="/connexions")
+
+@socketio.on("disconnect",namespace="/connexions")
+def ws_disconnect():
+    c = db.decr('connected')
+    socketio.emit('msg', {'counter': c}, namespace="/connexions")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
